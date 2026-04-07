58c7216c7c70de92a45cf002c68ae656a0ce2a31
dnm10 <deeptinm10@gmail.com>
2026-04-08 00:44:24 +0530

    client api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import toast  from 'react-hot-toast'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: '/http://13.201.40.154/api',
   timeout: 30000,
   headers: { 'Content-Type': 'application/json' },
 })
